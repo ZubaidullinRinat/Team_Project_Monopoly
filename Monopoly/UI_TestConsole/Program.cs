4df62ad27c8da6dee6146ba0710d6e8314506930
beheltcarrot <beheltcarrot@gmail.com>
2016-12-06 22:22:14 +0300

    DataProcess added + Console fore debug

---
+++ Monopoly/UI_TestConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using Logic.DataProcess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repository r = new Repository();
+            Console.WriteLine(r.test());
+            Console.ReadKey();
+        }
+    }
+}
