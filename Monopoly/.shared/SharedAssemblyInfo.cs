230184214673f6408c700a8cc117084395662010
root <root@DESKTOP-NK5C3ID>
2016-12-06 16:54:17 +0300

    Mvvm Base Structure

---
+++ Monopoly/.shared/SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("SomeName")]
+[assembly: AssemblyProduct("Mopoly team project")]
+[assembly: AssemblyCopyright("Copyright © SomeName 2016")]
+[assembly: AssemblyTrademark("SomeName")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
---
+++ Monopoly/UI_Main/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("UI_Main")]
+[assembly: AssemblyDescription("")]
